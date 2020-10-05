0704684546c59bda837a1a77ef3459183a6055b4
= <=>
2020-10-05 12:18:29 -0600

    arrglo error en la variable io

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,18 @@
 const {io} = require('../index');
 let  users = []; 
 io.on('connection', client => {
-    socket.on('adduser', function (user) { 
+    io.on('adduser', function (user) { 
         socket.user = user; 
         users[user] = user; 
         console.log(users); 
     }); 
     
-    socket.on('disconnect', function () { 
+    io.on('disconnect', function () { 
         console.log('User: ' + users[socket.user] + ' has disconnected');
         delete users[socket.user]; console.log(users) 
     });
 
-    socket.on('update', function () {
+    io.on('update', function () {
         users[user] = user; 
         console.log('Current users: ', users); 
     }); 
