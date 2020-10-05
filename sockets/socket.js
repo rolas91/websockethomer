90b8add0f0821f73935cc7f203cfbbaf7b3309d6
= <=>
2020-10-05 12:23:53 -0600

    cambio logica del socket

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,18 +1,18 @@
 const {io} = require('../index');
 let  users = []; 
 io.on('connection', client => {
-    io.on('adduser', function (user) { 
+    client.on('adduser', function (user) { 
         socket.user = user; 
         users[user] = user; 
         console.log(users); 
     }); 
     
-    io.on('disconnect', function () { 
+    client.on('disconnect', function () { 
         console.log('User: ' + users[socket.user] + ' has disconnected');
         delete users[socket.user]; console.log(users) 
     });
 
-    io.on('update', function () {
+    client.on('update', function () {
         users[user] = user; 
         console.log('Current users: ', users); 
     }); 
