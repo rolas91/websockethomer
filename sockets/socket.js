36fd2f8e194fdc64bd7a05fd8cd1f0eb5b43b9d0
= <=>
2020-10-06 12:29:43 -0600

    agrego debug en socket

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,16 +1,18 @@
 const {io} = require('../index');
 const activeUsers = new Set();
 io.on('connection', socket => {
-   
+   console.log('usuario conectado')
     socket.on('adduser', (data) => {
         socket.userId = data;
         activeUsers.add(data)
         io.emit('adduser',[...activeUsers]);
+        console.log('usuario activo');
     }); 
     
     socket.on('disconnect', () => { 
         activeUsers.delete(socket.userId);
         io.emit("user disconnected", socket.userId);
+        console.log('usuario desconectado');
     });
 
    
