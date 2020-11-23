ebcc6757b15d9f478fcb97d62aafaa5ec31e5d8c
Rolando Sanchez <rsanchez@numu.digital>
2020-11-23 16:09:04 -0600

    empiezo pruebas para el endpoint de nearBy 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,8 +4,5 @@ const {nearBy} = require('../controllers/homerProvider');
 
 
 router.post('/search', nearBy);
-router.post('/probando',(req, res)=>{
-    res.status(200).json({message:"exito"});
-})
 
 module.exports = router;
