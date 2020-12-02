72276d2274c9813a9851f979072fc9b11b4987f7
Rolando Sanchez <rsanchez@numu.digital>
2020-12-02 03:52:55 -0600

    agrego la ruta para crear las ordenes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 let express = require('express');
 let router = express.Router();
-const {nearBy} = require('../controllers/homerProvider');
+const {nearBy,createService} = require('../controllers/homerProvider');
 
 
 router.post('/search', nearBy);
+router.post('/orders/create',createService);
 
 module.exports = router;
