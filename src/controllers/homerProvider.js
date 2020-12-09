832e2791e0d3d43f00631fdf8e069c70a7eb982c
Rolando Sanchez <rsanchez@numu.digital>
2020-12-09 08:56:53 -0600

    corrijo los estados en ordenes

--- src/controllers/homerProvider.js
+++ src/controllers/homerProvider.js
@@ -60,10 +60,13 @@ module.exports.deleteProvider = async(ui) => {
 
 module.exports.createOrders = async(req, res) => {
     try {
-        
+        let address = '';
         const {clientUi, nameClient, productUi, productName, stateServiceId, date, hour,location, lat, lng} = req.body;
         console.log('coord',lat, lng);
-        //let address = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=AIzaSyBofvEOcrzbxSfBA7LTFSypr5SX3TT94Dk&sensor=false');
+        let googleinfo = await fetch('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&key=AIzaSyBofvEOcrzbxSfBA7LTFSypr5SX3TT94Dk&sensor=false');
+        googleinfo.map(result => {
+            address = result.results.formatted_address
+        })
         let newService = await Order.create({
             clientUi: clientUi, 
             nameClient: nameClient, 
@@ -72,7 +75,7 @@ module.exports.createOrders = async(req, res) => {
             stateServiceId: stateServiceId, 
             date: date, 
             hour: hour,
-            location:location,
+            location:address,
             lat:lat,
             lng:lng
         });
