a25c3534e761943b1bc8c673ae187e0b0801b4c0
Rolando Sanchez <rsanchez@numu.digital>
2021-01-07 07:45:51 -0600

    agrego nuevo estado en las ordenes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,6 @@ const {nearBy,createOrders, ordersEnd} = require('../controllers/homerProvider')
 
 router.post('/search', nearBy);
 router.post('/orders/create',createOrders);
-router.post('/orders/endorders',ordersEnd);
+router.post('/orders/changestate',ordersEnd);
 
 module.exports = router;
