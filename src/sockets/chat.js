13861a14982406b98e846c761ff03f534575572c
Rolando Sanchez <rsanchez@numu.digital>
2021-01-12 11:22:28 -0600

    agrego socket para el correo

---
+++ src/sockets/chat.js
@@ -0,0 +1,15 @@
+const {io} = require('../../index');
+io.on('connection', (socket) => {
+    socket.on('disconnect', function(){
+      io.emit('users-changed', {user: socket.nickname, event: 'left'});   
+    });
+   
+    socket.on('set-nickname', (nickname) => {
+      socket.nickname = nickname;
+      io.emit('users-changed', {user: nickname, event: 'joined'});    
+    });
+    
+    socket.on('add-message', (message) => {
+      io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+    });
+});
