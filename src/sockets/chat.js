73d22f8a29cb1fc53663ea1dfd73c3eeb1be29cc
Rolando Sanchez <rsanchez@numu.digital>
2021-01-12 12:52:51 -0600

    test

--- src/sockets/chat.js
+++ src/sockets/chat.js
@@ -3,12 +3,3 @@ socket.on('disconnect', function(){
   io.emit('users-changed', {user: socket.nickname, event: 'left'});   
 });
 
-socket.on('set-nickname', (nickname) => {
-  socket.nickname = nickname;
-  io.emit('users-changed', {user: nickname, event: 'joined'});    
-});
-
-socket.on('add-message', (message) => {
-  console.log(message);
-  io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
-});
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -76,6 +76,14 @@ io.on('connection', socket => {
         socket.broadcast.emit('chat:typing',data);
     });
     
+    socket.on('set-nickname', (nickname) => {
+        socket.nickname = nickname;
+        io.emit('users-changed', {user: nickname, event: 'joined'});    
+    });
+    socket.on('add-message', (message) => {
+        console.log(message);
+        io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
+    });
     
     socket.on('disconnect', () => { 
         homerProvider.deleteProvider(socket.userId).then(response => {
