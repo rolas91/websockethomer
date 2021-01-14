841b1205abdd7f460b3bb288f8c68b54b765486e
Rolando Sanchez <rsanchez@numu.digital>
2021-01-14 16:33:36 -0600

    agrego contador

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -5,32 +5,32 @@ const activeUsers = [];
 let countdown = 10;
 
 
-function countDown( minutes, seconds )
-{
-    var element, endTime, hours, mins, msLeft, time;
+// function countDown( minutes, seconds )
+// {
+//     var element, endTime, hours, mins, msLeft, time;
 
-    function twoDigits( n )
-    {
-        return (n <= 9 ? "0" + n : n);
-    }
+//     function twoDigits( n )
+//     {
+//         return (n <= 9 ? "0" + n : n);
+//     }
 
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
-            console.log("Time is up!");
-        } else {
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            console.log(( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds()));
-            io.to(`${data.id}`).emit('getCountDown', { count : ( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds())});
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
-    }
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-    updateTimer();
-}
+//     function updateTimer()
+//     {
+//         msLeft = endTime - (+new Date);
+//         if ( msLeft < 1000 ) {
+//             console.log("Time is up!");
+//         } else {
+//             time = new Date( msLeft );
+//             hours = time.getUTCHours();
+//             mins = time.getUTCMinutes();
+//             console.log(( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds()));
+//             io.to(`${data.id}`).emit('getCountDown', { count : ( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds())});
+//             setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
+//         }
+//     }
+//     endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
+//     updateTimer();
+// }
 
 
 
@@ -58,7 +58,7 @@ io.on('connection', socket => {
     
     socket.on('getCountDown', (data) => {
         socket.join(`${data.id}`)
-        setInterval(() => {
+       
             var  endTime, hours, mins, msLeft, time;
 
             function twoDigits( n )
@@ -82,7 +82,6 @@ io.on('connection', socket => {
             }
             endTime = (+new Date) + 1000 * (60*10 + 0) + 500;
             updateTimer();
-        },1000);
     });
 
     socket.on('getordersbyproviders', (data) => { 
