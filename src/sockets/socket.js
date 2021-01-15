e723f4cf17f1128766ca5e28d212ad969a86091b
Rolando Sanchez <rsanchez@numu.digital>
2021-01-15 10:53:14 -0600

    reparaciones

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -100,6 +100,7 @@ io.on('connection', socket => {
         socket.join(`${data.id}`)
         setInterval(() => {
             homerProvider.getOrderByClient(socket.userId).then(result => {
+                console.log(result)
                 io.to(`${data.id}`).emit('getordersbyclients',result)
             });
         },2000);
