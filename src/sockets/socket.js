bc4f0dc2629667475d628df34efb8454f77f0540
Rolando Sanchez <rsanchez@numu.digital>
2021-01-22 05:56:36 -0600

    agrego cambios

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -57,6 +57,7 @@ io.on('connection', socket => {
     }); 
     
     socket.on('getCountDown', (data) => {
+        console.log("estoy dentro")
             var  endTime, hours, mins, msLeft, time;
             setTimeout(() =>{
                 homerProvider.getOrderByProvider(socket.userId).then(result => {           
