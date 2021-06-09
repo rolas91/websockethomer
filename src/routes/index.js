23c19411d5bbbde94fdefc4d881daf04c2ae457f
Rolando Sanchez <rolando@laescala.cl>
2021-06-09 11:25:46 -0600

    agrego ruta de prueba

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,15 +7,18 @@ const {
   getMessage,
   getOrderCancelByProvider,
   addProvider2,
-  providerOneSignal
+  providerOneSignal,
 } = require("../controllers/homerProvider");
 
 router.post("/search", nearBy);
+router.post("/test", (req, res) => {
+  res.status(200).json({ code: "success", msg: "procesado correctamente" });
+});
 router.post("/orders/create", createOrders);
 router.post("/orders/changestate", ChangeOrders);
 router.post("/orders/endorders", getOrderCancelByProvider);
 router.post("/message/getmessages", getMessage);
 router.post("/provider/register", addProvider2);
-router.post("/provider/getonesignal",providerOneSignal)
+router.post("/provider/getonesignal", providerOneSignal);
 
 module.exports = router;
