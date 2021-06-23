d47efdac7aba66580bd854735e4bf9d803308636
Rolando Sanchez <rolando@laescala.cl>
2021-06-23 13:45:56 -0600

    testeando error en el contador

--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -128,60 +128,61 @@ io.on("connection", (socket) => {
     socket.join(`${data.id}`);
     setTimeout(() => {
       homerProvider.getOrderByProvider(socket.userId).then((result) => {
-        if (result.length > 0) {
-          for (let i = 0; i < result.length; i++) {
-            // if(result[i].isCount == 0 && result[i].isCountNow == 1) {
-            function twoDigits(n) {
-              return n <= 9 ? "0" + n : n;
-            }
+        // if (result.length > 0) {
+        //   for (let i = 0; i < result.length; i++) {
+        //     // if(result[i].isCount == 0 && result[i].isCountNow == 1) {
+        //     function twoDigits(n) {
+        //       return n <= 9 ? "0" + n : n;
+        //     }
 
-            function updateTimer() {
-              msLeft = endTime - +new Date();
-              if (msLeft < 1000) {
-                console.log("Time is up!");
-              } else {
-                time = new Date(msLeft);
-                hours = time.getUTCHours();
-                mins = time.getUTCMinutes();
+        //     function updateTimer() {
+        //       msLeft = endTime - +new Date();
+        //       if (msLeft < 1000) {
+        //         console.log("Time is up!");
+        //       } else {
+        //         time = new Date(msLeft);
+        //         hours = time.getUTCHours();
+        //         mins = time.getUTCMinutes();
                 
-                mytimer = (hours ? hours + ":" + twoDigits(mins) : mins) +
-                ":" +
-                twoDigits(time.getUTCSeconds()),
+        //         mytimer = (hours ? hours + ":" + twoDigits(mins) : mins) +
+        //         ":" +
+        //         twoDigits(time.getUTCSeconds()),
 
-                // console.log(data.id,( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds()));                
+        //         // console.log(data.id,( hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds()));                
                
-                setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
-                return mytimer;
-              }
-            }
-            endTime = +new Date() + 1000 * (60 * 10 + 0) + 500;
-            // updateTimer();
-            let results = [];
-                results.push({
-                  providerId: result[i].providerId,
-                  id: result[i].id,
-                  clientUi: result[i].clientUi,
-                  nameClient: result[i].nameClient,
-                  productUi: result[i].productUi,
-                  productName: result[i].productName,
-                  status: result[i].status,
-                  isCancel: result[i].isCancel,
-                  isCount: result[i].isCount,
-                  isCountNow: result[i].isCountNow,
-                  date: result[i].date,
-                  hour: result[i].hour,
-                  location: result[i].location,
-                  lat: result[i].lat,
-                  lng: result[i].lng,
-                  onesignal: result[i].onesignal,
-                  count:updateTimer()
+        //         setTimeout(updateTimer, time.getUTCMilliseconds() + 500);
+                
+        //         return mytimer;
+        //       }
+        //     }
+        //     endTime = +new Date() + 1000 * (60 * 10 + 0) + 500;
+        //     // updateTimer();
+        //     let results = [];
+        //         results.push({
+        //           providerId: result[i].providerId,
+        //           id: result[i].id,
+        //           clientUi: result[i].clientUi,
+        //           nameClient: result[i].nameClient,
+        //           productUi: result[i].productUi,
+        //           productName: result[i].productName,
+        //           status: result[i].status,
+        //           isCancel: result[i].isCancel,
+        //           isCount: result[i].isCount,
+        //           isCountNow: result[i].isCountNow,
+        //           date: result[i].date,
+        //           hour: result[i].hour,
+        //           location: result[i].location,
+        //           lat: result[i].lat,
+        //           lng: result[i].lng,
+        //           onesignal: result[i].onesignal,
+        //           count:updateTimer()
                     
-                });
-                io.to(`${data.id}`).emit("getordersbyproviders", results);
-            // }
-          }
-        }
-        // io.to(`${data.id}`).emit('getordersbyproviders',result)
+        //         });
+        //         io.to(`${data.id}`).emit("getordersbyproviders", results);
+        //     // }
+        //   }
+        // }
+        io.to(`${data.id}`).emit('getordersbyproviders',result)
       });
     }, 1000);
   });
