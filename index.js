cf2a9acc0273c1bc130a03919370dfd259f40f64
Rolando Sanchez <rolando@laescala.cl>
2021-06-28 11:35:30 -0600

    edito el update agregando un parametro en el where

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/validaactiveprovider", function (req, res) {
 //     res.status(200).json(haversine(start, end,{ unit: 'mile'}));
 // });
 
-const router = require("./src/routes"); 
+const router = require("./src/routes");
 app.use("/api/v1", router);
 
 const server = app.listen(port, () => {
@@ -48,7 +48,6 @@ const server = app.listen(port, () => {
       console.log("Se ha producido un error", error);
     });
   cron.schedule("* * * * *", () => {
-    console.log("se ejecuta cron");
     changeState();
   });
   console.log(`connection is successful on port  ${port}`);
