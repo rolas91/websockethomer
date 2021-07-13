ecc1b9074c94062dd666856d1da6a88e40cc79bb
Rolando Sanchez <rolando@laescala.cl>
2021-07-13 16:56:49 -0600

    testeando agregacion de homer y productos

--- src/controllers/homerProvider.js
+++ src/controllers/homerProvider.js
@@ -25,6 +25,7 @@ module.exports.updateProvider = async (homerid, state, data) => {
        
        for (let i = 0; i < productsFound.length; i++) {
          console.log(productsFound[i].ui);
+         console.log(products.includes(productsFound[i].ui));
        }
         // for (let i = 0; i < productsFound.length; i++) {
         //   for (let j = 0; j < products.length; j++) {
