07bc90e1d0654fb8d0a5e63f892699ea139c3548
Rolando Sanchez <rolando@laescala.cl>
2021-08-30 17:38:06 -0600

    ultimos cambios

--- src/controllers/homerProvider.js
+++ src/controllers/homerProvider.js
@@ -285,7 +285,7 @@ module.exports.getOrderCancelByProvider = async (req, res) => {
     let response = await sequelize.query(
       `SELECT DISTINCT(productsproviders.providerId), orders.* FROM orders INNER JOIN
              productsproviders on productsproviders.ui = orders.productUi
-             where productsproviders.providerId = ${req.body.provider} and orders.status = 7 or orders.status = 6 or orders.status = 8`,
+             where productsproviders.providerId = ${req.body.provider} and (orders.status = 7 or orders.status = 6 or orders.status = 8) order by orders.id DESC`,
       {
         type: sequelize.QueryTypes.SELECT,
       }
