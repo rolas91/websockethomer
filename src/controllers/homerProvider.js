87bf56ef8810d30bc6b3c14d8a6cfde38ceb106f
Rolando Sanchez <rolando@laescala.cl>
2021-10-04 15:39:06 -0600

    ultimos cambios

--- src/controllers/homerProvider.js
+++ src/controllers/homerProvider.js
@@ -264,7 +264,7 @@ module.exports.createOrders = async (req, res) => {
       productUi: productUi,
       productName: productName,
       stateServiceId: stateServiceId,
-      date: moment(new Date(date), "YYYY-MM-DD"),
+      date: moment(date).format("YYYY-MM-DD"),
       hour: hour,
       hour_end: hour_end,
       location: location,
